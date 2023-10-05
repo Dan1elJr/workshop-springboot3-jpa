b2b548be7d707c829e26e315ccf7d13320279cdc
Daniel <danieljunior1903@gmail.com>
2023-10-05 13:47:54 -0400

    Category atualização

--- src/main/java/com/educandoweb/course/entities/Category.java
+++ src/main/java/com/educandoweb/course/entities/Category.java
@@ -13,8 +13,12 @@ import jakarta.persistence.Table;
 @Table(name ="tb_category")
 public class Category implements Serializable{
 	
-	private static final Long serialVersionUID = 1L;
 	
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
