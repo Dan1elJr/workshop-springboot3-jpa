a2be42e4345c5f5c20272f0912848354601de592
Daniel Junior <danieljunior1903@gmail.com>
2023-10-27 23:45:44 -0400

    atualização

--- src/main/java/com/educandoweb/course/config/TestConfig.java
+++ src/main/java/com/educandoweb/course/config/TestConfig.java
@@ -65,7 +65,7 @@ public class TestConfig implements CommandLineRunner {
 		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
 		
 		User u1 = new User(null, "Sarah Brown", "sara@gmail.com", "988888888", "123456"); 
-		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456");
+		User u2 = new User(null, "Alex Green", "alex@gmail.com", "967777777", "123456");
 		
 		Order o1 = new Order(null, Instant.parse("2019-06-20T19:53:07Z"),OrderStatus.PAID ,u1); 
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"),OrderStatus.WAITING_PAYMENT, u2); 
